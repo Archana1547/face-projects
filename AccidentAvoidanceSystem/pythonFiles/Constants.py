166b08722512ae3a3c25c86777a9c2f219815b56
Archana1547 <82135913+Archana1547@users.noreply.github.com>
2021-05-16 00:42:19 +0530

    Contains all the constant values
    Values such as Left and Right eye landmarks, Thresh hold value for ear etc

--- AccidentAvoidanceSystem/pythonFiles/Constants.py
+++ AccidentAvoidanceSystem/pythonFiles/Constants.py
@@ -2,13 +2,13 @@ import dlib
 import playsound
 import GetEarMar
 
-Threshold=0.5
-CFrames=40
+Threshold=0.5   #Threshold for ear
+CFrames=40      # No of frames for which threshold is .5
 AoN=False
 cntr=0
 cntrA=0
-AEyePointsL=[37,38,39,40,41,42]
-AEyePointsR=[43,44,45,46,47,48]
+AEyePointsL=[37,38,39,40,41,42]   #Left Eye Landmarks
+AEyePointsR=[43,44,45,46,47,48]    #Right eye Landmarks 
 
 
 def play():
